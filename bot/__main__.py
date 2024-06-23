713786ebfb0f09b856a6082fbe516f3d6761ba39
dgaakash <138712879+dgaakash@users.noreply.github.com>
2024-06-24 01:46:34 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,32 +1,5 @@
 from bot import TelegramBot, logger
-from aiohttp import web
-import os
-import asyncio
-from pyrogram import idle
-
-async def handle(request):
-    return web.Response(text="Hello, Render!")
-
-async def init_web_app():
-    app = web.Application()
-    app.router.add_get('/', handle)
-    port = int(os.environ.get('PORT', 5000))
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, '0.0.0.0', port)
-    await site.start()
-
-async def run_telegram_bot():
-    logger.info('Initializing...')
-    await TelegramBot.start()  # Start the bot
-    await idle()  # Keep the bot running until interrupted
-    await TelegramBot.stop()  # Stop the bot when idle is interrupted
-
-async def main():
-    await asyncio.gather(
-        init_web_app(),
-        run_telegram_bot()
-    )
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    logger.info('Initializing...')
+    TelegramBot.run()
