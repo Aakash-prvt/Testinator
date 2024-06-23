e826456f722c01ed8b9f4238ea129081f0f136a7
dgaakash <138712879+dgaakash@users.noreply.github.com>
2024-06-23 13:53:16 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,6 +1,7 @@
 from bot import TelegramBot, logger
 from flask import Flask
 import os
+import threading
 
 app = Flask(__name__)
 
@@ -8,7 +9,20 @@ app = Flask(__name__)
 def home():
     return 'Hello, Render!'
 
-if __name__ == '__main__':
+def run_flask():
     port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+def run_telegram_bot():
     logger.info('Initializing...')
-    TelegramBot.run(host='0.0.0.0', port=port)
+    TelegramBot.run()
+
+if __name__ == '__main__':
+    flask_thread = threading.Thread(target=run_flask)
+    bot_thread = threading.Thread(target=run_telegram_bot)
+
+    flask_thread.start()
+    bot_thread.start()
+
+    flask_thread.join()
+    bot_thread.join()
