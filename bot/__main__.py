33364b8f429b35f58081989ba8600d1969c7dfc5
dgaakash <138712879+dgaakash@users.noreply.github.com>
2024-07-04 19:27:04 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,21 +1,5 @@
-# Initialize Flask app
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello, I'm alive!"
-
-def run_flask():
-    # Get the port from the environment variable
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+from bot import TelegramBot, logger
 
 if __name__ == '__main__':
     logger.info('Initializing...')
-
-    # Start the Flask app in a separate thread
-    flask_thread = Thread(target=run_flask)
-    flask_thread.start()
-
-    # Run the Telegram bot
     TelegramBot.run()
