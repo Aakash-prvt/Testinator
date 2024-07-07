968ef6026a9a979f1881c1f8a1b870aa780b1210
dgaakash <138712879+dgaakash@users.noreply.github.com>
2024-07-07 10:26:26 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,14 @@
 from bot import TelegramBot, logger
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    logger.info('Initializing...')
-    TelegramBot.run()
+    port = int(os.environ.get("PORT", 5000))
+    TelegramBot.run(host='0.0.0.0', port=port)
+
